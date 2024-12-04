74be886adaf3ec1503e213132396c724984f0eb9
SaiMartande <saimartande@gmail,com>
2024-12-04 13:10:32 +0530

    Extra line delete

--- Demo.java
+++ Demo.java
@@ -3,6 +3,8 @@ package Demo_repo;
 public class Demo {
     public void demo() {
         System.out.println("In demo function");
+        System.out.println("Add line in main");
+
     }
 
     public static void main(String[] args) {
