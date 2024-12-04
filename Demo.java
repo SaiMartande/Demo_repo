97babd4ca6b996d1bc1d65cfafe792f67dafcfac
SaiMartande <saimartande@gmail,com>
2024-12-04 13:03:46 +0530

    Add demo function

--- Demo.java
+++ Demo.java
@@ -1,6 +1,10 @@
 package Demo_repo;
 
 public class Demo {
+    public void demo() {
+        System.out.println("In demo function");
+    }
+
     public static void main(String[] args) {
         System.out.println("In demo Repo's main");
     }
